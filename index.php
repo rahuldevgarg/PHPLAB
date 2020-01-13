30dfcd72e086722dd2dc04c9ec0970293b2eddcb
Rahul Dev Garg <rdg042000@gmail.com>
2020-01-13 11:51:12 +0530

    My First PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// Single Line Comment
+/* Multiline 
+    Comment
+    */
+
+# Scripting Format Comment
+echo "This is my First PHP Code";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>This is html embedded in php.</p>
+</body>
+</html>
